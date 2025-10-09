cd5fe71399df3b9f0c368f7797c8f003ddee9131
lucasdino <lucasdionisopoulos@gmail.com>
2025-10-09 16:05:59 -0700

    getting verl-agent ready

---
+++ runscripts/setups/local_setup.sh
@@ -0,0 +1,2 @@
+# Since I'm storing data differently on device vs. how I'll handle on cluster
+export ALFWORLD_DATA="$~/code/seq-decision/sequential_decision_processors/data/alfworld"
---
+++ runscripts/setups/setup.sh
@@ -0,0 +1,3 @@
+# Download data for our envs
+alfworld-download -f
+export ALFWORLD_DATA="$~/.cache/alfworld"
